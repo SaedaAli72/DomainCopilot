8d8cd6ca8df38b6d1b5a369178155114a1ec2bbf
Saedaali72 <Saedaali72@gmail.com>
2026-08-29 13:17:32 +0300

    feat(application): add core interfaces for LLM, vector store, and embeddings

--- DomainCopilot.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DomainCopilot.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ DomainCopilot.Application/Interfaces/ILlmClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainCopilot.Application.Interfaces
+{
+    internal interface ILlmClient
+    {
+    }
+}
